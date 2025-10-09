4800afa2375928e319f3bebe2643003c1615e53b
Vikas Joshi <vikasjoshi10889@gmail.com>
2025-10-09 18:32:36 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost:3000", 
+    "http://localhost:3001",
+    "https://zerodha-clone-frontend-kohl.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
